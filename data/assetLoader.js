77318cb847cc587888a941d3d6a36dc40a0dcc6a
Jonas Schulz <js97.sw@gmail.com>
2026-03-13 17:15:20 +0100

    Added missing explanations

--- data/assetLoader.js
+++ data/assetLoader.js
@@ -40,11 +40,11 @@ const questionDicts = [
     },
     {
         question: "Enemy Killing Spree vor 10:00",
-        explanation: "",
+        explanation: "Das Announcement 'Enemy Killing Spree!' ist vor 10:00 zu hören.",
     },
     {
         question: "Enemy Legendary vor 15:00",
-        explanation: "",
+        explanation: "Das Announcement 'An enemy is legendary' ist vor 15:00 zu hören.",
     },
     {
         question: "2 Meme-Champions werden gepickt",
@@ -64,83 +64,87 @@ const questionDicts = [
     },
     {
         question: "Recall wird durch Minions unterbrochen",
-        explanation: "",
+        explanation: "Zählt erst ab 4 Sekunden Channel-Zeit.",
     },
     {
         question: "Double Ace",
-        explanation: "",
+        explanation: "Alle 10 Champions sind gleichzeitig tot.",
     },
     {
         question: "Ace vor 20:00",
-        explanation: "",
+        explanation: "Alle 5 Champions eines Teams sind gleichzeitig tot (vor 20:00).",
     },
     {
         question: "Dritter Baron getötet",
-        explanation: "",
+        explanation: "Auch 2:1 Barons zählt.",
     },
     {
         question: "Zweiter Elder getötet",
-        explanation: "",
+        explanation: "Auch 1:1 Elder zählt.",
     },
     {
         question: "3 gefundene Matches hintereinander abgelehnt",
-        explanation: "",
+        explanation: "3 Mal 'Accept' hat nicht gereicht. Auch ein verlassener Champ Select zählt als 1.",
     },
     {
         question: "Champ Select wurde verlassen",
-        explanation: "",
+        explanation: "Selbsterklärend",
+    },
+    {
+        question: "System Notification erscheint",
+        explanation: "Im Champ-Select erscheint die System Notification, dass das Spiel gleich losgehen wird."
     },
     {
         question: "1 Nexus stirbt, während irgendwo anders gekämpft wird",
-        explanation: "",
+        explanation: "Es verteidigt keiner den Nexus, aber es wird irgendwo gekämpft, und der Nexus fällt.",
     },
     {
-        question: "Yasuo/Warwick geht 0/5",
-        explanation: "",
+        question: "Ein Meme-Champion geht 0/5",
+        explanation: "Meme-Champions: Warwick, Yasuo, Caitlyn, Mel, Heimerdinger, Ivern",
     },
     {
         question: "Spieler geht 0/10",
-        explanation: "",
+        explanation: "Irgendjemand schafft 0/10 K/D (beliebige Assists).",
     },
     {
         question: "Team verliert Baron Buff ohne zu pushen",
-        explanation: "",
+        explanation: "Sobald Minions empowered wurden und damit Schaden an Minions/Towern gemacht wurden, 'wurde gepusht'.",
     },
     {
         question: "Gegner hat 20 Kills",
-        explanation: "",
+        explanation: "Mindestens 20",
     },
     {
         question: "Ally hat 15 Tode",
-        explanation: "",
+        explanation: "Mindestens 15",
     },
     {
         question: "3 Allies haben 10 Tode",
-        explanation: "",
+        explanation: "Mindestens 3 Allies, die mindestens 10 Tode haben",
     },
     {
         question: "50 Minuten geknackt",
-        explanation: "",
+        explanation: "1 Spiel ueberschreitet 50:00 Spielzeit",
     },
     {
         question: "4:3 Drakes getötet",
-        explanation: "",
+        explanation: "Nachdem beide Teams 3 Drakes haben, wird der siebte Drake (Soul) getötet.",
     },
     {
         question: "3 Fail Smites in 1 Game",
-        explanation: "",
+        explanation: "Fail Smite = Jungler smitet, bevor der Threshold erreicht wurde. Nicht zu smiten (weil zu spät) ist kein Fail Smite.",
     },
     {
         question: "Baron/Drake ohne Smite gestohlen",
-        explanation: "",
+        explanation: "Gestohlen = die Systemnachricht im Chat sagt das so.",
     },
     {
         question: "30 Sekunden lange Chase",
-        explanation: "",
+        explanation: "Irgendjemand läuft länger als 30s seinem Gegner hinterher. Hinterherlaufen = in Sichtweite sein und in die gleiche Richtung laufen.",
     },
     {
         question: "Opfer Flash",
-        explanation: "",
+        explanation: "Jemand benutzt den Flash und stirbt dann ausschliesslich an Schaden, der bereits vor dem Flash auf dem Weg zu ihm war.",
     },
 ]
 
