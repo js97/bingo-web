d07335bc3208ca975da947e06f633237e9f02a1b
Jonas Schulz <js97.sw@gmail.com>
2026-03-17 20:19:17 +0100

    Added favicon

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+var favicon = require('serve-favicon');
 
 const { questions, questionDicts } = require("./data/assetLoader.js");
 
 const app = express();
 const PORT = 3000;
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // Serve static files from /public
 app.use(express.static(path.join(__dirname, "public")));
